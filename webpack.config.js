b6a678c68a09ea538bbd1e05f01b169d591537cf
Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
2025-09-26 15:24:23 +0200

    Do not version css and js + add observed radio saintfe

--- webpack.config.js
+++ webpack.config.js
@@ -46,7 +46,7 @@ Encore
 
     .enableSourceMaps(!Encore.isProduction())
     // enables hashed filenames (e.g. app.abc123.css)
-    .enableVersioning(Encore.isProduction())
+    //.enableVersioning(Encore.isProduction())
 
     // configure Babel
     // .configureBabel((config) => {
